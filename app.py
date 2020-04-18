eeb5edad01715580061d3f7b52a414b6b2ea3ea7
Adel elmala <adel.elmala2025@gmail.com>
2020-04-18 23:36:59 +0200

    v2 bd

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import librosa.display
 import glob
 import gc
 import functools
-
+import soundfile as sf
 
 
 
@@ -100,7 +100,7 @@ class audioSpectogram():
         if len(self.samples.shape) == 2: #Check if setreo and convert it to mono
             self.samples = np.mean(self.samples, axis=1)
         
-        print(self.samples.shape)    
+        # print(self.samples.shape)    
         print('time (secs): ',self.lenght)
 
         # check if the length of the song is more than 1 min, if so ,clip it (now it's trivial  T_T )
@@ -121,14 +121,14 @@ class audioSpectogram():
         self.tonnetzHash = self.hashFile(ft2,self.hashMode)
         self.chromaHash = self.hashFile(ft3,self.hashMode)
         print(spect,ft1,ft2,ft3)
-        print('spect: ',self.hashFile(spect,self.hashMode))
-        print('mffc: ',self.hashFile(ft1,self.hashMode))
-        print('tonnetz: ',self.hashFile(ft2,self.hashMode))
-        print('chroma: ',self.hashFile(ft3,self.hashMode))
+        print('spect: ',self.spectHash)
+        print('mffc: ',self.mffcHash)
+        print('tonnetz: ',self.tonnetzHash)
+        print('chroma: ',self.chromaHash)
         print("mode:",self.hashMode)
-        print(type(self.spectHash))
-        print(self.spectHash - self.spectHash)
-        print(self.path) 
+        # print(type(self.spectHash))
+        # print(self.spectHash - self.spectHash)
+        # print(self.path) 
 
     def getData(self):
         return (self.spectHash,self.mffcHash,self.tonnetzHash,self.chromaHash,self.times,self.frequencies,self.spectrogram)    
@@ -145,7 +145,7 @@ class audioSpectogram():
         # plt.show()
         X = librosa.stft(self.samples)
         Xdb = librosa.amplitude_to_db(abs(X))
-        plt.figure(figsize=(14, 5))
+        # plt.figure(figsize=(14, 5))
         librosa.display.specshow(Xdb, sr=self.sample_rate)
         plt.tight_layout(pad = 0)
         spectFig = mktemp('.png')  # use temporary file
@@ -170,7 +170,7 @@ class audioSpectogram():
     def features(self):
         # mffcs (Featrue 1)
         mfccs = librosa.feature.mfcc(y=self.samples, sr=self.sample_rate)
-        plt.figure(figsize=(10, 6))
+        # plt.figure(figsize=(10, 6))
         librosa.display.specshow(mfccs)
         plt.tight_layout(pad = 0)
         # mffcFig = 'mffc.png'
@@ -180,7 +180,7 @@ class audioSpectogram():
 
         #  tonal centroid (feature 2)
         tonnetz = librosa.feature.tonnetz(y=self.samples, sr=self.sample_rate)
-        plt.figure(figsize=(10, 6))
+        # plt.figure(figsize=(10, 6))
         librosa.display.specshow(tonnetz)
         plt.tight_layout(pad = 0)
         # tonnetzFig = 'tonnetz.png'
@@ -190,7 +190,7 @@ class audioSpectogram():
         
         # chroma (feature 3 )
         chroma = librosa.feature.chroma_cqt(self.samples, sr=self.sample_rate)
-        plt.figure(figsize=(10, 6))
+        # plt.figure(figsize=(10, 6))
         librosa.display.specshow(chroma)
         plt.tight_layout()
         # chromaFig = 'chroma.png'
@@ -209,7 +209,7 @@ class audioSpectogram():
 
 def loopFolder(path:str,extension:str):
     Files = glob.glob(path + "/*" + extension) # list of all (mp3/or else) files in the folder
-    print(Files)
+    # print(Files)
     return Files
 
 def stripName(path:str):
@@ -217,9 +217,9 @@ def stripName(path:str):
     songName = names[-1]
     songNameLst = songName.split('.')
     songName = songNameLst[0]
-    print(names)
-    print(songName)
-    print(songNameLst)
+    # print(names)
+    # print(songName)
+    # print(songNameLst)
     return songName
 
 def  generateTxt(inputMixedSong : 'audioSpectogram',hashMode:str="perception"):
@@ -233,23 +233,23 @@ def  generateTxt(inputMixedSong : 'audioSpectogram',hashMode:str="perception"):
         songName =  stripName(files[i])
 
         print("NAME: ",songName)
-        spectFile = open("spectrogram-output/"+songName+".txt","w")    # needs to be filled with arrays
+        # spectFile = open("spectrogram-output/"+songName+".txt","w")    # needs to be filled with arrays
         song = audioSpectogram(files[i],i,hashMode)
         spectHash,mffcHash,tonnetzHash,chromaHash,times,frequencies,spectrogram = song.getData()        
         
         hashFile.write("Name:{},spectHash:{},mffcHash:{},tonnetzHash:{},chromaHash:{}\n".format(songName,spectHash,mffcHash,tonnetzHash,chromaHash))
-        spectFile.close()
+        # spectFile.close()
         spectScore = 100 - (spectHash - MixedSpectHash)
         mffcScore = 100 - (mffcHash - MixedMffcHash)
         tonnetzScore = 100 - (tonnetzHash - MixedTonnetzHash)
         chromaScore = 100 - (chromaHash - MixedChromaHash)
         totalScore = (spectScore + mffcScore + tonnetzScore + chromaScore)/4
-        print("totalScore:",totalScore)
-        print("spectScore:",spectScore)
-        print("mffcScore:",mffcScore)
-        print("tonnetzScore:",tonnetzScore)
-        print("chromaScore:",chromaScore)
-        similarityScore.write("{},{},{},{},{}\n".format(songName,spectScore,mffcScore,tonnetzScore,totalScore))
+        # print("totalScore:",totalScore)
+        # print("spectScore:",spectScore)
+        # print("mffcScore:",mffcScore)
+        # print("tonnetzScore:",tonnetzScore)
+        # print("chromaScore:",chromaScore)
+        similarityScore.write("{},{},{},{},{},{}\n".format(songName,spectScore,mffcScore,tonnetzScore,chromaScore,totalScore))
         
         
         del song
@@ -260,10 +260,7 @@ def  generateTxt(inputMixedSong : 'audioSpectogram',hashMode:str="perception"):
 # # loopFolder('/home/adel/dsp-task4','.mp3')
 # hehe = audioSpectogram('/home/adel/dsp-task4/songs/Sia_Alive_05.mp3',99,"difference")
 
-hehe = audioSpectogram('/home/adel/dsp-task4/songs/Sam Smith_Too_Good_At_Goodbyes_vocals_05.mp3',99,"wavelet")
 
-
-generateTxt(hehe,"wavelet")
 
 def readFromTxt():
     hashFile = open("hashing-output/hashing.txt",'r')
@@ -288,9 +285,34 @@ def similarity(inputMixedSong : 'audioSpectogram'):
 
     searchIn = extractFromline(readFromTxt())
 
+def mix(path1:str,path2:str,ratio:float):
+    x1 , sr1 = librosa.load(path1, duration=60.0)
+    x2 , sr2 = librosa.load(path2, duration=60.0)
+    x1=x1*ratio
+    x2=x2*(1-ratio)
+    x=x1+x2
+    temp = mktemp('.wav')
+    
+    sf.write(temp,x ,sr1)
+    # print(temp)
+    sound = AudioSegment.from_wav(temp)
+
+    sound.export('mixed.mp3', format="mp3")
+
+
+mix('songs/Sia_Alive_05.mp3','songs/Sam Smith_Too_Good_At_Goodbyes_music_05.mp3',0.5)
+
+
+# mix('songs3/franksinatra_my_way_music_28.mp3','songs3/Birdy_strange_birds_10_vocals.mp3',0)
+
 
 # songslist = readFromTxt()
 # extractedsongsData = extractFromline(songslist)
 # print(extractedsongsData)
 # print(len(extractedsongsData))
 # print(imagehash.ImageHash("ad1dc053ca32f956"))
+
+
+
+inputSong = audioSpectogram('/home/adel/dsp-task4/mixed.mp3',99,"wavelet")
+generateTxt(inputSong,"wavelet")
--- mix.py
+++ mix.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 """
 Created on Sat Apr 18 01:19:21 2020
 
@@ -9,6 +7,7 @@ import librosa
 import IPython as ip
 import soundfile as sf
 from pydub import AudioSegment
+
 audio_data1 = 'songs/Sia_Alive_05.mp3'
 audio_data2 = 'songs/Sam Smith_Too_Good_At_Goodbyes_vocals_05.mp3'
 x1 , sr1 = librosa.load(audio_data1, duration=60.0)
